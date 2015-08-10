b23cbacaf15d4d3bc68df71708422693be970d2c
Edil Ashimov <edilashimov@Edils-MacBook-Pro.local>
2015-08-10 10:50:53 -0500

    Initial Commit

---
+++ IntergalacticTravelSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IntergalacticTravelSwift
+//
+//  Created by Edil Ashimov on 8/10/15.
+//  Copyright (c) 2015 Edil Ashimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
